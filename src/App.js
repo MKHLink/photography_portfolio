53b0289b4e5a492b016f713aa13b3aed2c37d4cf
MKHLink <[likhonhasankomol@gmail.com]>
2022-12-25 16:35:41 -0500

    added contact form and made app into a SPW

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Nav from './components/Nav';
 import ConatctForm from './components/Contact';
 
 function App() {
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -24,13 +27,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-          <ConatctForm></ConatctForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+      {!contactSelected ? (
+      <>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
+      </>
+      ) : (
+        <ConatctForm></ConatctForm>
+      )}
       </main>
     </div>
   );
